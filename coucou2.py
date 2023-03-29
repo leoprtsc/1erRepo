a5c8d03bf86bdaac954cfbfe0114dc4d1f4d856c
Leo Protais <leo.protais@consort.group.com>
2023-03-29 17:03:43 +0200

    Changement de cc2 dans une nouvelle branche

--- coucou2.py
+++ coucou2.py
@@ -1,4 +1,21 @@
 x = 2 + 2
 print(x)
 
-#salut léoooooooooooooooooooooo
+# salut léoooooooooooooooooooooo
+
+Leo = x**2
+print(Leo)
+
+
+def test():
+    print()
+    x = input()
+    if x == "expert git":
+        print("Léo est : " + str(x) + " effectivement")
+    else:
+        print("mauvaise réponse :(")
+
+
+test()
+
+## 50% de chance que j'arrive pas a envoyé au repo github
